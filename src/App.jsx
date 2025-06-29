3c4927ef4abde478dc6e32d732d5c087863bf2e5
khushbupatel05 <khushbupatel0009@gmail.com>
2025-06-29 17:11:07 +0530

    commit for form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Form from './Components/Form'
+import UserList from './Components/UserList'
 
 const App = () => {
+
+  const [users, setUsers] = useState([]);
+   const addUsers = (User) =>{
+      setUsers([...users , User])
+   }
+    useEffect(() => {
+        localStorage.setItem("users", JSON.stringify(users));
+    }, [users])
   return (
     <>
-      <Form />
+      <Form addUsers={addUsers} />
+      <UserList />
     </>
   )
 }
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,92 +1,182 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-const Form = () => {
+const Form = ({ editUser, addUsers, updateUser }) => {
   const [input, setInput] = useState({
-    name: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-    rate: "",
+    name: '', email: '', course: '', gender: '', password: '', confirmPassword: '',
   });
+
+  const [errors, setErrors] = useState({});
+
+  const formref = useRef(null)
+
+  useEffect(() => {
+    if (editUser) {
+      setInput(editUser);
+    } else {
+      setInput({
+        name: '', email: '', course: '', gender: '', password: '', confirmPassword: '',
+      });
+    }
+  }, [editUser]);
 
   const handleChange = (e) => {
     setInput({ ...input, [e.target.id]: e.target.value });
+    setErrors({ ...errors, [e.target.id]: "" });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    let showError = {};
+
+    let validationErrors = {};
 
     if (input.name.trim() === "") {
-      showError.name = "Enter Your Name";
+      validationErrors.name = "Enter Your Name !";
     }
-    if (input.password.trim() === "") {
-      showError.message = "Enter Valid Review!";
+
+    if (input.email.trim() === "") {
+      validationErrors.email = "Enter Your Email !";
     }
-    if (input.confirmPassword.trim() === "") {
-      showError.date = "Select Valid Date!";
+
+    if (!input.course) {
+      validationErrors.course = "Select Your Course !";
     }
-    if (input.rate.trim() === "") {
-      showError.rate = "Select Rating!";
+
+    if (!input.gender) {
+      validationErrors.gender = "Please select your gender";
     }
+
+    if (input.password.trim().length < 8) {
+      validationErrors.password = "Enter a Valid and Strong Password !";
+    }
+
+    if (input.password.trim() !== input.confirmPassword.trim()) {
+      validationErrors.confirmPassword = "Password and Confirm Password Should Be Same !";
+    }
+
+    setErrors(validationErrors);
+
+    if (Object.keys(validationErrors).length > 0) return;
+
+    if (editUser) {
+      updateUser(input);
+    } else {
+      addUsers({ ...input, id: Date.now() });
+    }
+
+    setInput({
+      name: '', email: '', course: '', gender: '', password: '', confirmPassword: '',
+    });
+
+    setErrors({});
   };
 
   return (
-    <div className="min-h-screen py-10 px-4 flex items-center justify-center">
-      <div className="w-full max-w-md p-8 rounded-xl shadow-2xl">
-        <form onSubmit={handleSubmit} className="space-y-6">
-          <h2 className="text-center text-2xl font-semibold text-gray-800">Crud Local Storage</h2>
+    <div className='container mx-auto my-4'>
+      <h1 className='text-3xl text-center font-semibold my-12'>Form Validation</h1>
 
-          <div>
-            <label htmlFor="name" className="block text-sm font-medium text-gray-700">Your Name</label>
-            <input type="text" id="name" value={input.name} onChange={handleChange} className="mt-1 w-full p-3 border border-gray-300 rounded-xl bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Enter your name" />
-          </div>
+      <form ref={formref} onSubmit={handleSubmit} className="max-w-sm mx-auto">
 
-          <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Enter Email Id:</label>
-            <input type="text" id="email" value={input.email} onChange={handleChange} className="mt-1 w-full p-3 border border-gray-300 rounded-xl bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Enter your EmailId" />
-          </div>
+        <div className="mb-5">
+          <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Your Name</label>
+          <input onChange={handleChange} value={input.name} type="text" id="name"  className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"   placeholder="Name"  />
+          {errors.name && <p className="text-red-500 text-sm mt-1">{errors.name}</p>}
+        </div>
 
-          <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">PassWord:</label>
-            <input type="password" id="password" value={input.password} onChange={handleChange} className="mt-1 w-full p-3 border border-gray-300 rounded-xl bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-400" />
-          </div>
+ 
+        <div className="mb-5">
+          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your Email</label>
+          <input
+            onChange={handleChange}
+            value={input.email}
+            type="email"
+            id="email"
+            className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+            placeholder="Email" />
+          {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email}</p>}
+        </div>
 
-          <div>
-            <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">Confirm PassWord:</label>
-            <input type="password" id="confirmPassword" value={input.confirmPassword} onChange={handleChange} className="mt-1 w-full p-3 border border-gray-300 rounded-xl bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-400" />
-          </div>
+        <div className="mb-5">
+          <label htmlFor="course" className="block mb-2 text-sm font-medium text-gray-900">Select Course</label>
+          <select
+            onChange={handleChange}
+            value={input.course}
+            id="course"
+            className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+          >
+            <option value="">Select Course</option>
+            <option value="1">Full Stack Development</option>
+            <option value="2">Marketing</option>
+            <option value="3">UI / UX</option>
+            <option value="4">AI / ML</option>
+          </select>
+          {errors.course && <p className="text-red-500 text-sm mt-1">{errors.course}</p>}
+        </div>
 
-          <div>
-            <label htmlFor="Country" className="block text-sm font-medium text-gray-700"></label>
-            <select id="rate" value={input.rate} onChange={handleChange} className="mt-1 w-full p-3 border border-gray-300 rounded-xl bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-400" >
-              <option value={0}>Select Course</option>
-              <option value="1">Full-Stach Developer</option>
-              <option value="2">UI/UX</option>
-              <option value="3">AI/ML</option>
-              <option value="4"></option>
-            </select>
-          </div>
+        {/* Gender */}
+        <div className="flex gap-4 mb-4">
+          <label className="flex items-center gap-2 text-gray-900">
+            <input
+              type="radio"
+              id="gender"
+              name="gender"
+              value="Male"
+              onChange={handleChange}
+              checked={input.gender === "Male"}
+            />
+            Male
+          </label>
+          <label className="flex items-center gap-2 text-gray-900">
+            <input
+              type="radio"
+              id="gender"
+              name="gender"
+              value="Female"
+              onChange={handleChange}
+              checked={input.gender === "Female"}
+            />
+            Female
+          </label>
+        </div>
+        {errors.gender && <p className="text-red-500 text-sm mb-4">{errors.gender}</p>}
 
-          <div className='flex gap-3'>
-            <div className="flex items-center mb-4">
-              <input id="female" type="radio" defaultValue name="default-radio" className="w-4 h-4 text-blue-600" />
-              <label htmlFor="female" className="ms-2 text-sm font-medium">Female</label>
-            </div>
-            <div className="flex items-center">
-              <input defaultChecked id="male" type="radio" defaultValue name="default-radio" className="w-4 h-4 text-blue-600" />
-              <label htmlFor="male" className="ms-2 text-sm font-medium">Male</label>
-            </div>
-          </div>
+        
+        <div className="mb-5">
+          <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Your Password</label>
+          <input
+            onChange={handleChange}
+            value={input.password}
+            type="password"
+            id="password"
+            className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+            placeholder="Password"
+          />
+          {errors.password && <p className="text-red-500 text-sm mt-1">{errors.password}</p>}
+        </div>
 
-          <button type="submit" className="p-1 ps-3 pe-3 bg-cyan-700 ms-40 text-white font-semibold rounded hover:bg-cyan-800 bg-cyan-700transition-all">
-            Submit
-          </button>
-        </form>
-      </div>
+       
+        <div className="mb-5">
+          <label htmlFor="confirmPassword" className="block mb-2 text-sm font-medium text-gray-900">Repeat Password</label>
+          <input
+            onChange={handleChange}
+            value={input.confirmPassword}
+            type="password"
+            id="confirmPassword"
+            className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+            placeholder="Confirm Password"
+          />
+          {errors.confirmPassword && <p className="text-red-500 text-sm mt-1">{errors.confirmPassword}</p>}
+        </div>
+
+       
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5"
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
 
 export default Form;
-
